b70f4e1889c20783fdc1bfbb16dec7d98cf0d30e
Govind Rai <221b165@juetguna.in>
2025-01-28 11:29:20 +0530

    Delete Le1 directory

--- Le1/Mother.java
+++
@@ -1,7 +0,0 @@
-class Mother{
-    int x=5;
-    void show(){
-        System.out.println("Hey this show method from mother class");
-        System.out.println("And the value of x is: " + x);
-    }
-}
