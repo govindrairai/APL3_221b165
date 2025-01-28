f4a9ebac40c272cddd6d225bfe13cf83b9700aa7
Govind Rai <221b165@juetguna.in>
2025-01-28 11:20:39 +0530

    This is Lab Excercise 1

---
+++ Le1/Mother.java
@@ -0,0 +1,7 @@
+class Mother{
+    int x=5;
+    void show(){
+        System.out.println("Hey this show method from mother class");
+        System.out.println("And the value of x is: " + x);
+    }
+}
