298efc1cca6d705c8def96445a44d1020d1c733e
Govind Rai <221b165@juetguna.in>
2025-04-08 11:09:27 +0530

    This is lab excericise 6

---
+++ Le6/Test.java
@@ -0,0 +1,10 @@
+
+public class Test
+{
+	public static void main(String[] args) {
+	    Offering offering = new Coffee();
+	    offering = new Rum(offering);
+	    
+		System.out.println(offering.getName()+offering.getPrice());
+	}
+}
