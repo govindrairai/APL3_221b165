2c9d2b7bdf83d66e3ecdbaf3766f6a685d75aeff
Govind Rai <221b165@juetguna.in>
2025-05-06 11:26:57 +0530

    This is lab experiment 7

---
+++ Le7/IndianSwan.java
@@ -0,0 +1,13 @@
+public class IndianSwan implements Swan {
+    public void swim() {
+        System.out.println("Gracefully swimming on the lake");
+    }
+
+    public void sing() {
+        System.out.println("Singing melodiously");
+    }
+
+    public void eat() {
+        System.out.println("Eating aquatic plants");
+    }
+}
